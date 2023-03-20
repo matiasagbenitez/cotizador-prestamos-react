09c087617246c60b579ae289a8b22b23c3f3a1ca
Matías <matiasagbenitez@gmail.com>
2023-03-20 10:57:05 -0300

    Componente input para ingresar el dinero requerido

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+function Header() {
+    return (
+        <h1 className="text-3xl font-bold text-center text-gray-500">
+        ¿Cuánto <span className="text-indigo-600">dinero </span> necesitas?
+      </h1>
+    )
+}
+
+export default Header;
