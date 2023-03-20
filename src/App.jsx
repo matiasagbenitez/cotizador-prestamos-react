7d3631d75cbe178b72e6172eee074bba2cea2611
Matías <matiasagbenitez@gmail.com>
2023-03-20 11:24:30 -0300

    Proyecto finalizado

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
-import { useState } from "react"
-import Header from "./components/Header"
+import { useState, useEffect } from "react";
+import Header from "./components/Header";
 import Button from "./components/Button";
-import { formatearDinero } from "./helpers"
+import { formatearDinero, calcularTotalPagar } from "./helpers";
 
 function App() {
-
   const [cantidad, setCantidad] = useState(10000);
+  const [meses, setMeses] = useState(6);
+  const [total, setTotal] = useState(0);
+  const [pago, setPago] = useState(0);
+
+  useEffect(() => {
+    setTotal(calcularTotalPagar(cantidad, meses));
+  }, [cantidad, meses]);
+
+  useEffect(() => {
+    setPago(total / meses);
+  }, [total]);
+
   const MIN = 0;
   const MAX = 20000;
   const STEP = 100;
@@ -27,24 +38,17 @@ function App() {
   }
 
   return (
-    <div className="my-10 max-w-lg mx-auto bg-white shadow p-10">
-      
+    <div className="my-7 max-w-lg mx-auto bg-white shadow py-5 px-10 rounded-lg">
       <Header />
 
       <div className="flex justify-between my-7">
-        <Button 
-          operador="-" 
-          fn={handleClickDecremento}
-        />
-        
-        <Button 
-          operador="+" 
-          fn={handleClickIncremento}
-        />
+        <Button operador="-" fn={handleClickDecremento} />
+
+        <Button operador="+" fn={handleClickIncremento} />
       </div>
 
-      <input 
-        type="range" 
+      <input
+        type="range"
         className="w-full h-10 bg-gray-200 accent-lime-500 hover:accent-lime-600"
         onChange={handleChange}
         min={MIN}
@@ -57,8 +61,37 @@ function App() {
         {formatearDinero(cantidad)}
       </p>
 
+      <h2 className="text-xl font-bold text-gray-500 text-center mt-3">
+        Elige un <span className="text-indigo-600">plazo</span> a pagar
+      </h2>
+
+      <select
+        className="mt-5 w-full p-2 bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm text-center text-gray-500 font-semibold"
+        value={meses}
+        onChange={(e) => setMeses(+e.target.value)}
+      >
+        <option value="6">6 meses</option>
+        <option value="12">12 meses</option>
+        <option value="24">24 meses</option>
+      </select>
+
+      <div className="space-y-2 bg-gray-50 p-5">
+        <h2 className="text-xl font-bold text-gray-500 text-center mt-3">
+          Resumen<span className="text-indigo-600"> de pagos</span>
+        </h2>
+
+        <p className="text-gray-500 text-center font-bold">{meses} meses</p>
+
+        <p className="text-gray-500 text-center font-bold">
+          {formatearDinero(total)} total a pagar
+        </p>
+
+        <p className="text-gray-500 text-center font-bold">
+          {formatearDinero(pago)} mensuales
+        </p>
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/helpers/index.js
+++ src/helpers/index.js
@@ -6,4 +6,34 @@ const formatearDinero = (cantidad) => {
     return formatter.format(cantidad);
 }
 
-export { formatearDinero };
+const calcularTotalPagar = (cantidad, plazo) => {
+    let total;
+
+    if (cantidad < 5000) { 
+        total = cantidad * 1.5;
+    } else if (cantidad >= 5000 && cantidad < 10000) {
+        total = cantidad * 1.4;
+    } else if (cantidad >= 10000 && cantidad < 15000) {
+        total = cantidad * 1.3;
+    } else {
+        total = cantidad * 1.2;
+    }
+
+    switch (plazo) {
+        case 6:
+            total = total * 1.1;
+            break;
+        case 12:
+            total = total * 1.2;
+            break;
+        case 24:
+            total = total * 1.3;
+            break;
+        default:
+            break;
+    }
+
+    return total;
+}
+
+export { formatearDinero, calcularTotalPagar };
